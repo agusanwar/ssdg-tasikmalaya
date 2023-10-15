3a9f814ab2b003b52b2908f72cbf3b44358a900b
agusanwar <agusanwar@gmail.com>
2023-10-15 10:17:14 +0700

    footer components

--- app/components/Footer/index.jsx
+++ app/components/Footer/index.jsx
@@ -1,18 +1,76 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 
 const Index = () => {
-  return (
-    <footer className="w-full border z-10 border-t-[rgb(51,53,63)] border-l-transparent border-r-transparent text-white">
-      <div className="py-5 flex items-center justify-between px-5">
-        <h1 className="text-white mb-5 text-sm font-bold">
-          <Image src='/images/logo.webp' alt="image hero" width={50} height={50} />
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-yellow-400">
-            SSDG KORWIL TASIKMALAYA
-          </span>
-        </h1>
-        <span className="underline text-sm sm:text-xs underline-offset-2 mt-5">{new Date().getFullYear()}&copy;ssdgtasikmalaya</span>
+  return (   
+    <footer className="bg-color-dark rounded-lg  shadow dark:bg-gray-900">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex md:items-center sm:justify-between">
+          <Link href="#" className="flex items-center mb-4 sm:mb-0">
+            <Image src="/images/logo.webp" width={50} height={80} className="h-8 mr-1" alt="ssdg Logo" />
+            <h2 className="text-white text-center text-sm font-extrabold ">
+              <span className="text-transparent bg-clip-text bg-gradient-to-r text-2xl from-yellow-600 to-color-indigo">
+                SSDG TASIKMALAYA
+              </span>
+            </h2>
+          </Link>
+          <ul className="flex flex-wrap justify-center items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <div className="flex items-center justify-between"> 
+              <Link href="https://www.tiktok.com/@ssdg.tasikmalaya" className="mr-9 text-neutral-800 dark:text-neutral-200">
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  width="16" 
+                  height="16" 
+                  fill="currentColor" 
+                  className="bi bi-tiktok" 
+                  viewBox="0 0 16 16"> 
+                  <path 
+                    d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z"
+                  /> 
+                </svg>
+              </Link>
+              <Link href="https://web.facebook.com/ssdg.korwiltasikmalaya" className="mr-9 text-neutral-800 dark:text-neutral-200">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 24 24">
+                  <path
+                    d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
+                  />
+                </svg>
+              </Link>
+              <Link href="https://www.instagram.com/ssdg.korwiltasikmalaya" className="mr-9 text-neutral-800 dark:text-neutral-200">
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  width="16" 
+                  height="16" 
+                  fill="currentColor" 
+                  className="bi bi-instagram" 
+                  viewBox="0 0 16 16">
+                  <path 
+                    d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"
+                  />
+                </svg>
+              </Link>
+              <Link href="#!" className="text-neutral-800 dark:text-neutral-200">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 24 24">
+                  <path
+                    d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+                  />
+                </svg>
+              </Link>
+            </div>
+          </ul>
+        </div>
+        <hr className=" flex my-6 border-gray-200 items-center justify-center sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 SSDGTASIKMALAYA™. All Rights Reserved.</span>
       </div>
     </footer>
   );
