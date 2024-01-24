edd0f25a0607eeb6abc352f7cbab8b5378c57aa0
agusanwar <agusanwar@gmail.com>
2024-01-24 16:41:40 +0700

    update event harlah

--- app/new-informasi/page.jsx
+++ app/new-informasi/page.jsx
@@ -43,7 +43,7 @@ const NewInformation = () => {
                 </div>
                 <div className="flex-grow md:text-left text-center mt-6 md:mt-0 leading-loose">
                   <h2 className="text-white text-lg title-font font-medium">FAJAR</h2>
-                  <p className="leading-relaxed text-base">Registrasi Pendaftaran</p>
+                  <p className="leading-relaxed text-base">Pendaftaran Peserta</p>
                   <Link href="https://api.whatsapp.com/send?phone=+6281323084642" className="mt-3 text-color-yellow hover:text-color-primary inline-flex items-center">
                   <svg className="w-6 h-6 mr-5 text-color-yellow dark:text-color-yellow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
                     <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
