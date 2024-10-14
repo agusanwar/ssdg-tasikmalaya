7d2da3cd187ba5ebb5fbbdb11bf13e78c25401df
agusanwar <agusanwar13.aa@gmail.com>
2024-10-14 09:32:31 +0700

    update information

---
+++ app/components/index.jsx
@@ -0,0 +1,9 @@
+
+
+const Indonesia = 1;
+const Autralia = 1;
+
+
+if(oman == kyrgyzstan){
+    return 'Indonesia Lolos to 16 besar AFC Cup';
+}
--- app/new-informasi/page.jsx
+++ app/new-informasi/page.jsx
@@ -3,18 +3,20 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const NewInformation = () => {
+
+  
   return (
     <div className="infomation w-full items-center justify-center py-10 px-10">  
       <h1 className="text-white text-xl md:text-4xl text-center font-extrabold ">
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-600 to-orange-200">
-       HARLAH SSDG KORWIL TASIKMALAYA
+       LIPUTAN SITU LENGKONG, PANJALU CIAMIS
         </span>
       </h1>
       <div className="md:grid md:grid-cols-2 md:p-4 gap-x-8 md:gap-x-8 ">
         <div className="flex w-full h-auto justify-center rounded-3xl shadow hover:shadow-2xl hover:shadow-amber-700 cursor-pointer text-color-primary hover:text-color-yellow transition-all">
           <Image
             className="px-4 py-8 items-center justify-center rounded-r-lg transition-all"
-            src="/images/new_information/harlah.webp" 
+            src="/images/new_information/panjalu.webp" 
             width={700} 
             height={700} 
             alt="image wakaf"
@@ -23,14 +25,14 @@ const NewInformation = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">  
           <h2 className="text-xl md:text-2xl text-center md:text-start py-4 font-extrabold ">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-yellow-500">
-              HARLAH SSDG Korwil Tasikmalaya ke-3
+              INFORMATION
              </span>
           </h2>
           <p className="text-justify leading-loose lg:text-md ">
-            HARLAH SSDG Korwil Tasikmalaya ke-3 akan diselenggarakan Insya Allah pada hari Sabtu, 17 Februari 2024 yang berlokasi di Agra Hot Springs Wisata Curug Batu Badak Hanoman, Tasikmalaya, Jawa Barat
+           Kegiatan SDG di Korwil Tasikmalaya pada kesempatan ini, melakukan kegiatan liputan di Situ Lengkong Panjalu Ciamis.
           </p>
           <p className="text-justify mt-5 lg:text-md">
-            Untuk informasi lebih lanjut bisa menghubungi:
+            Adapun untuk informasi lebih lanjut bisa menghubungi:
           </p>
           <div className='flex flex-col'>
            <div className="py-4 md:w-full">
@@ -42,13 +44,13 @@ const NewInformation = () => {
               </svg>
                 </div>
                 <div className="flex-grow md:text-left text-center mt-6 md:mt-0 leading-loose">
-                  <h2 className="text-white text-lg title-font font-medium">FAJAR</h2>
+                  <h2 className="text-white text-lg title-font font-medium">-</h2>
                   <p className="leading-relaxed text-base">Pendaftaran Peserta</p>
                   <Link href="https://api.whatsapp.com/send?phone=+6281323084642" className="mt-3 text-color-yellow hover:text-color-primary inline-flex items-center">
                   <svg className="w-6 h-6 mr-5 text-color-yellow dark:text-color-yellow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
                     <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
                   </svg>
-                  +62 882-2900-9460
+                  -
                   </Link>
                 </div>
               </div>
@@ -64,29 +66,45 @@ const NewInformation = () => {
               </svg>
                 </div>
                 <div className="flex-grow md:text-left text-center mt-6 md:mt-0 leading-loose">
-                  <h2 className="text-white text-lg title-font font-medium">DIKA PRATAMA</h2>
+                  <h2 className="text-white text-lg title-font font-medium">-</h2>
                   <p className="leading-relaxed text-base">Informasi Pendaftaran</p>
                   <Link href="https://api.whatsapp.com/send?phone=+6281323084642" className="mt-3 text-color-yellow hover:text-color-primary inline-flex items-center">
                   <svg className="w-6 h-6 mr-5 text-color-yellow dark:text-color-yellow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
                     <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
                   </svg>
-                  +62 819-3050-9090
+                 -
                   </Link>
                 </div>
               </div>
             </div>
           </div>
-          <p className="text-justify py-10 md:text-lg text-md leading-loose">
-            Adapun Include Yang akan didapatkan oleh peserta HARLAH SSDG Korwil Tasikmalaya ke-3: 
-          </p>
-          <div className="grid justify-center items-center md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-y-8 gap-x-4">
+            <h2 className="text-xl md:text-2xl text-center md:text-start py-4 font-extrabold ">
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-yellow-500">
+                REGISTRASI
+              </span>
+            </h2>
+          <div className="mt-4 md:mt-2 text-left flex flex-col h-full">  
+            <p className="text-justify leading-loose lg:text-md">
+              Untuk Link Registrasi LIPUTAN PANJALU SITU LENGKONG CIAMIS, Silahkan isi DATA diri dengan lengkap 
+            </p>
+            <p className="text-justify py-10 md:text-lg text-md leading-loose">
+              Adapun Include Yang akan didapatkan oleh peserta HARLAH SSDG Korwil Tasikmalaya ke-3: 
+            </p>
+          </div>
+          <h2 className="text-xl md:text-2xl text-center md:text-start py-4 font-extrabold ">
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-yellow-500">
+                INCLUDE
+              </span>
+            </h2>
+          <div className="grid justify-center items-center md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-y-4  gap-x-4">
+          
             <div className="flex flex-wrap justify-center items-center bg-black rounded-2xl p-4 shadow hover:shadow-2xl shadow-amber-500 hover:shadow-amber-700 cursor-pointer text-color-primary hover:text-color-yellow transition-all">     
               <p className="text-right mb-5 md:text-lg text-md leading-loose">
                 1. Kartu Access Peserta
               </p>
               <Image
                 className="shadow rounded max-w-full h-auto align-middle border-none"
-                src="/images/new_information/id_peserta.webp" 
+                src="/images/new_information/id_access.webp" 
                 width={300} 
                 height={250} 
                 alt="image voucher"
@@ -94,49 +112,11 @@ const NewInformation = () => {
             </div>
              <div className="flex flex-wrap justify-center items-center bg-black rounded-2xl p-4 shadow hover:shadow-2xl shadow-amber-500 hover:shadow-amber-700 cursor-pointer text-color-primary hover:text-color-yellow transition-all">     
                <p className="text-justify mb-5 md:text-lg text-md leading-loose">
-                2. Voucher Coffee Break dan Makan
+                2. 1 X Coffee Break 
               </p>
               <Image
                 className="shadow rounded max-w-full h-auto align-middle border-none mb-10" 
-                src="/images/new_information/coffe.webp" 
-                width={300} 
-                height={250} 
-                alt="image voucher"
-              />
-              <Image
-                className="shadow rounded max-w-full h-auto align-middle border- mb-10"
-                src="/images/new_information/m1.webp" 
-                width={300} 
-                height={250} 
-                alt="image voucher"
-              />
-              <Image
-                className="shadow rounded max-w-full h-auto align-middle border-none"
-                src="/images/new_information/m2.webp" 
-                width={300} 
-                height={250} 
-                alt="image voucher"
-              />
-            </div>
-             <div className="flex flex-wrap justify-center items-center bg-black rounded-2xl p-4 shadow hover:shadow-2xl shadow-amber-500 hover:shadow-amber-700 cursor-pointer text-color-primary hover:text-color-yellow transition-all">     
-              <p className="text-justify mb-5 md:text-lg text-md leading-loose">
-                3. Souvenir
-              </p>
-              <Image
-                className="shadow rounded max-w-full h-auto align-middle border-none"
-                src="/images/new_information/souvenir.webp" 
-                width={300} 
-                height={250} 
-                alt="image voucher"
-              />
-            </div>
-             <div className="flex flex-wrap justify-center items-center bg-black rounded-2xl p-4 shadow hover:shadow-2xl shadow-amber-500 hover:shadow-amber-700 cursor-pointer text-color-primary hover:text-color-yellow transition-all">     
-              <p className="text-justify mb-5 md:text-lg text-md leading-loose">
-                4. Tenda DLL
-              </p>
-              <Image
-                className="shadow rounded max-w-full h-auto align-middle border-none"
-                src="/images/new_information/tenda.webp" 
+                src="/images/new_information/coffee_break.webp" 
                 width={300} 
                 height={250} 
                 alt="image voucher"
