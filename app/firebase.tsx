64fa8f09887ea3503c231d1d39e173c4eb5adda7
agusanwar <agusanwar13.aa@gmail.com>
2024-10-29 13:52:06 +0700

    create register

--- app/firebase.tsx
+++ app/firebase.tsx
@@ -2,14 +2,13 @@ import firebase from "firebase/compat/app";
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCb5_xkuJPhRvU1DWXbplw4IXk8s9w4Spk",
-    authDomain: "newssdgtasik.firebaseapp.com",
-    projectId: "newssdgtasik",
-    storageBucket: "newssdgtasik.appspot.com",
-    messagingSenderId: "751420052495",
-    appId: "1:751420052495:web:c2b6e726ae87ab3f7f3f7a",
-    measurementId: "G-GHJQ9RQ8HD"
-  };
-
-  const app = firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCb5_xkuJPhRvU1DWXbplw4IXk8s9w4Spk",
+  authDomain: "newssdgtasik.firebaseapp.com",
+  projectId: "newssdgtasik",
+  storageBucket: "newssdgtasik.appspot.com",
+  messagingSenderId: "751420052495",
+  appId: "1:751420052495:web:81ad8c619da286bd7f3f7a",
+  measurementId: "G-F6NFVTETPT"
+};
+const app = firebase.initializeApp(firebaseConfig);
 export const db  = firebase.firestore();
--- app/registrasi/page.jsx
+++ app/registrasi/page.jsx
@@ -6,21 +6,21 @@ import {collection, addDoc} from 'firebase/firestore';
 const Registrasi = () => { 
     // handling user
     const [name, setName] = useState("");
-    const [alamat, setAlamat] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
+    const [alamat, setAlamat] = useState("");
 
     // create db
     const dbref = collection(db, "registrasi")
     // handling add function
     const add = async ()=>{
         if(name.length === 0 || email.length === 0 || phone.length === 0){
-          
+          alert("silahkan lengakapi data anda")
         }
         else{
             try {
                 await addDoc(dbref, {name:name, email:email, phone:phone, alamat:alamat})
-                alert("succeess add data")
+                alert("Success, Anda berahasil Registrasi")
                 setName("")
                 setEmail("")
                 setPhone("")
@@ -49,7 +49,7 @@ const Registrasi = () => {
                         onChange={(e) =>setName(e.target.value)} 
                         required 
                         className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
-                         placeholder="Enter nama" 
+                        placeholder="Enter nama" 
                     />
                 </div>
               </div>
@@ -59,9 +59,9 @@ const Registrasi = () => {
                     <input type="text" 
                       value={email}
                       onChange={(e) =>setEmail(e.target.value)}
-                        required 
-                        className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
-                         placeholder="Enter email" 
+                      required 
+                      className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
+                      placeholder="Enter email" 
                     />
                 </div>
               </div>
@@ -69,11 +69,11 @@ const Registrasi = () => {
                 <label className="text-gray-800 text-sm mb-2 block">Nomor Handphone</label>
                 <div className="relative flex items-center">
                     <input type="text" 
-                       value={phone}
-                       onChange={(e) =>setPhone(e.target.value)}  
-                        required 
-                        className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
-                         placeholder="Enter no handphone" 
+                      value={phone}
+                      onChange={(e) =>setPhone(e.target.value)}  
+                      required 
+                      className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
+                      placeholder="Enter no handphone" 
                     />
                 </div>
               </div>
@@ -85,14 +85,13 @@ const Registrasi = () => {
                        onChange={(e) =>setAlamat(e.target.value)}  
                         required 
                         className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
-                         placeholder="Enter alamat" 
+                        placeholder="Enter alamat" 
                     />
                 </div>
               </div>
 
-              <div className="!mt-8">
+              <div className="button !mt-8">
                 <button
-                   type="submit" 
                    onClick={add}
                     className="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
                     >
