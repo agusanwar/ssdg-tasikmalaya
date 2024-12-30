7a86a443085d9640464267de0f7b01e1c7766b0c
agusanwar <agusanwar13.aa@gmail.com>
2024-12-30 14:25:03 +0700

    informasi

--- app/informasi-liputan/page.jsx
+++ app/informasi-liputan/page.jsx
@@ -14,7 +14,7 @@ const NewInformation = () => {
         <div className="flex w-full h-auto justify-center rounded-3xl shadow hover:shadow-2xl hover:shadow-amber-700 cursor-pointer text-color-primary hover:text-color-yellow transition-all">
           <Image
             className="px-4 py-8 items-center justify-center rounded-r-lg transition-all"
-            src="/images/new_information/panjalu.webp" 
+            src="/images/new_information/situ-lengkong.webp" 
             width={700} 
             height={700} 
             alt="image wakaf"
@@ -42,13 +42,13 @@ const NewInformation = () => {
               </svg>
                 </div>
                 <div className="flex-grow md:text-left text-center mt-6 md:mt-0 leading-loose">
-                  <h2 className="text-white text-lg title-font font-medium">-</h2>
+                  <h2 className="text-white text-lg title-font font-medium">Kang Yogi A Yogaswara</h2>
                   <p className="leading-relaxed text-base">Pendaftaran Peserta</p>
                   <Link href="https://api.whatsapp.com/send?phone=+6281323084642" className="mt-3 text-color-yellow hover:text-color-primary inline-flex items-center">
                   <svg className="w-6 h-6 mr-5 text-color-yellow dark:text-color-yellow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
                     <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
                   </svg>
-                  -
+                  0853-5399-1777
                   </Link>
                 </div>
               </div>
@@ -64,13 +64,13 @@ const NewInformation = () => {
               </svg>
                 </div>
                 <div className="flex-grow md:text-left text-center mt-6 md:mt-0 leading-loose">
-                  <h2 className="text-white text-lg title-font font-medium">-</h2>
+                  <h2 className="text-white text-lg title-font font-medium">Kang Iqbal</h2>
                   <p className="leading-relaxed text-base">Informasi Pendaftaran</p>
                   <Link href="https://api.whatsapp.com/send?phone=+6281323084642" className="mt-3 text-color-yellow hover:text-color-primary inline-flex items-center">
                   <svg className="w-6 h-6 mr-5 text-color-yellow dark:text-color-yellow" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
                     <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
                   </svg>
-                 -
+                 0813-2788-0229
                   </Link>
                 </div>
               </div>
@@ -86,42 +86,22 @@ const NewInformation = () => {
               Untuk Link Registrasi LIPUTAN PANJALU SITU LENGKONG CIAMIS, Silahkan isi DATA diri dengan lengkap.
             </p>
             <p className="text-justify mt-4 lg:text-md">
-            link: <Link href={"https://docs.google.com/forms/d/e/1FAIpQLSc0HG-Oa4rNKMkqoxngFj0KXI1VjCwJIFvLZ15Kfo86Xl4oEg/viewform?usp=sf_link"} className="mr-12 text-blue-600  underline hover:text-amber-500 transition-all">G-form Registrasi Liputan</Link>
+            link: <Link href={"https://ssdg-tasikmalaya.vercel.app/registrasi-situlengkong"} className="mr-12 text-blue-600  underline hover:text-amber-500 transition-all">Web-Form Registrasi Liputan</Link>
             </p>
-          </div>
-          <h2 className="text-xl md:text-2xl text-center md:text-start mt-4 py-4 font-extrabold ">
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-yellow-500">
-                INCLUDE
-              </span>
-            </h2>
-            <p className="text-justify mb-6 md:text-lg text-md">
-              Adapun Include Yang akan didapatkan oleh peserta pada Liputan Situ Lengkong Panjalu Ciamis: 
-            </p>
-          <div className="grid justify-start items-start md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-y-4  gap-x-4">
-            <div className="flex flex-wrap justify-start items-center bg-black rounded-2xl p-4 shadow hover:shadow-2xl shadow-amber-500 hover:shadow-amber-700 cursor-pointer text-color-primary hover:text-color-yellow transition-all">     
-              <p className="text-right mb-5 md:text-lg text-md leading-loose">
-                1. Kartu Access Peserta
+            <h2 className="text-xl md:text-2xl text-center md:text-start mt-4 py-4 font-extrabold ">
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-yellow-500">
+                  Catatan 
+                </span>
+              </h2>
+              <p className="text-justify mb-6 md:text-lg text-md">
+                1. Untuk pendaftaran bisa di koordinasikan dengan korwil masing-masing.
               </p>
-              <Image
-                className="shadow rounded max-w-full h-auto align-middle border-none"
-                src="/images/new_information/id_access.webp" 
-                width={300} 
-                height={250} 
-                alt="image voucher"
-              />
-            </div>
-             <div className="flex flex-wrap items-start bg-black rounded-2xl p-4 shadow hover:shadow-2xl shadow-amber-500 hover:shadow-amber-700 cursor-pointer text-color-primary hover:text-color-yellow transition-all">     
-               <p className="text-justify mb-5 md:text-lg text-md leading-loose">
-                2. 1 X Coffee Break 
+              <p className="text-justify mb-6 md:text-lg text-md">
+                2. Tiket Wisata ke Nusa dan Transportasinya bisa dibeli secara mandiri,
               </p>
-              <Image
-                className="shadow rounded max-w-full h-auto align-middle border-none mb-10" 
-                src="/images/new_information/coffee_break.webp" 
-                width={300} 
-                height={250} 
-                alt="image voucher"
-              />
-            </div>   
+              <p className="text-justify mb-6 md:text-lg text-md">
+                3. Tidak menyediakan Penginapan dan Makan.
+              </p>
           </div>
         </div>
       </div>
@@ -139,7 +119,7 @@ const NewInformation = () => {
               </p>
               <Image
                 className="shadow rounded max-w-full h-auto align-middle border-none"
-                src="/images/new_information/flow-registrasi.webp" 
+                src="/images/new_information/flow-chart.webp" 
                 width={650} 
                 height={450} 
                 alt="image voucher"
@@ -162,7 +142,30 @@ const NewInformation = () => {
           </div>
           </div>
       </div>
-       
+
+      <div className='py-12'>
+      <h1 className="text-white text-xl md:text-3xl text-start font-bold mb-4">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-600 to-orange-200">
+          Panitia Liputan Situ Lengkong Panjalu Ciamis
+          </span>
+        </h1>
+        <div className="grid md:grid-cols-2 item-start sm:grid-cols-2 grid-cols-1 gap-4 px-4">
+          <div>
+              {/* <div className="flex flex-wrap justify-center items-start bg-black rounded-2xl py-4 px-4 shadow hover:shadow-2xl shadow-amber-500 hover:shadow-amber-700 cursor-pointer text-color-primary hover:text-color-yellow transition-all">     
+                <p className="text-start mb-5 md:text-lg text-md leading-loose">
+                  Tata Cara Registrasi Liputan
+                </p>
+                <Image
+                  className="shadow rounded max-w-full h-auto align-middle border-none"
+                  src="/images/new_information/flow-chart.webp" 
+                  width={650} 
+                  height={450} 
+                  alt="image voucher"
+                />
+              </div> */}
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
