7bea633d39e5dcf2dd7d476a75268a4800a8671a
agusanwar <agusanwar13.aa@gmail.com>
2025-01-01 12:13:32 +0700

    data peserta

--- app/data-peserta-situgede/page.jsx
+++ app/data-peserta-situgede/page.jsx
@@ -4,13 +4,13 @@ import React from "react";
 import NewTable from '../components/NewTable'
 
 
-const DataPesertaSitulengkong = () => {
+const DataPesertaSitugede= () => {
   return(
     <div className="col-span-7 bg-[#121212] px-5 py-5 min-h-screen"> 
       <div className="flex items-left justify-left mb-5">
         <h1 className="text-white flex items-center justify-center text-center md:text-4xl sm:text-5xl text-2xl font-extrabold ">
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-orange-200">
-           LIST DATA LIPUTAN SITU LENGKONG
+           LIST DATA LIPUTAN SITU GEDE
           </span>
         </h1>
       </div>
@@ -21,6 +21,6 @@ const DataPesertaSitulengkong = () => {
   );
 };
 
-export default DataPesertaSitulengkong;
+export default DataPesertaSitugede
 
 
