e87d517b43969ee8e9727e6b5b402bcc055dd577
agusanwar <agusanwar13.aa@gmail.com>
2025-01-15 13:15:35 +0700

    table data situlengkong

--- app/components/TableJumlahPesertaSituLengkong/index.jsx
+++ app/components/TableJumlahPesertaSituLengkong/index.jsx
@@ -35,7 +35,7 @@ const columns = [
     createData('8', 'Korwil Sukabumi', '11 Anggota','-'),
     createData('9', 'Korwil Sumedang', '9 Anggota','-'),
     createData('10', 'Korwil Tangerang', '14 Anggota','-'),
-    createData('11',  'Korwil Tasikmalaya', ' Anggota','-'),
+    createData('11',  'Korwil Tasikmalaya', '34 Anggota','-'),
     createData('12', 'Korwil Rating Banjar', '5 Anggota','-'),
     createData('13', 'Fans SDG/Belum Masuk Korwil', '7 Anggota','-'),
   ];
